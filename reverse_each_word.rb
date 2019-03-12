ecdddbae9bf4a98afa9429fb1aad549eefb5d048
Adam S <adam.sarli@gmail.com>
2019-03-12 19:10:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split.collect{|word| word.reverse}.join(" ")
+  sentence.split.collect{|word| word.reverse}.join(" ")
 end 
 
 puts reverse_each_word("Hello there, how are you?")
