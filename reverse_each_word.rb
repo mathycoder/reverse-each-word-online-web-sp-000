c5fa4889b65b7d51f56f3cbf4e3d62e1fcbda8fe
Adam S <adam.sarli@gmail.com>
2019-03-12 19:05:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  words = sentence.split 
-  reversed = words.collect{|word| word.reverse}
+  sentence.split.collect{|word| word.reverse}
   reversed.join(" ")
 end 
 
